7efb18e5d517dc48c8346fd2a6d75f5699b8d582
mr geex <mamadkhane@gmail.com>
2026-09-23 22:38:04 +0800

    add unit test for BridgeB onlyOwner release()

--- contract/test/unit/CrossChain.test.js
+++ contract/test/unit/CrossChain.test.js
@@ -36,7 +36,25 @@ describe("Testing Transfer from chain A to chain B", () => {
       assert.equal(response, signerB.address);
     });
     it("should revert the same tx made twice", async () => {});
-    it("should revert the release() made by attacker", async () => {});
+    it("only owner can call release()", async () => {
+      const attacker = new ethers.Wallet(
+        "d8527d7f9437c7d90a8271d652439d6fba9f8678da47aa02ce644680aae47e17",
+        chainB,
+      );
+      const contractBAttacker = await contractB.connect(attacker);
+
+      console.log(`>>>>>${attacker.address}`);
+
+      await expect(
+        contractBAttacker.release(
+          "0xfbc654eae8a1cf25a50b472d5e2c0d67548fbf51747de872623d10a7d5cff3f0",
+          attacker.address,
+          {
+            value: ethers.parseEther("1"),
+          },
+        ),
+      ).to.be.revertedWithCustomError(contractB, "BridgeB_NotOwner");
+    });
     it("should revert tx if fee payments fail", () => {});
   });
 });
